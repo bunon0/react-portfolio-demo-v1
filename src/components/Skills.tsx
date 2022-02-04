1880939bef132bfb107542aa76ad3e64556e158a
bunon0 <86172592+Nobu-ur@users.noreply.github.com>
2022-02-04 13:00:24 +0900

    ✨ Skillコンポーネントの雛形を追加

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,3 +1,30 @@
+import styled from "styled-components";
+
 export const Skills: React.VFC = () => {
-  return <header>Header</header>;
+  return (
+    <SSkill>
+      <SContainer>
+        <SHeading>
+          <h2>Skills</h2>
+        </SHeading>
+      </SContainer>
+    </SSkill>
+  );
 };
+
+const SSkill = styled.div`
+  padding: 128px 0;
+  clear: both;
+`;
+
+const SContainer = styled.div`
+  margin-bottom: 32px;
+  text-align: center;
+`;
+
+const SHeading = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  text-align: center;
+  justify-content: center;
+`;
